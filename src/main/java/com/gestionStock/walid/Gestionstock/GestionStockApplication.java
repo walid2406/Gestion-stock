c0fce6d8778a6377d1c4db34168fb67780557ef3
walid.abboud.tech <walid.abboud.tech@gmail.com>
2022-02-15 08:25:43 +0100

    initial commit start projects

---
+++ src/main/java/com/gestionStock/walid/Gestionstock/GestionStockApplication.java
@@ -0,0 +1,13 @@
+package com.gestionStock.walid.Gestionstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockApplication.class, args);
+	}
+
+}
